a718ac9930fe8a4397909e2899d5960f6a4747bb
Georgy <pgv138@gmail.com>
2025-07-22 15:53:15 +0500

    add get_db()

--- app/db/database.py
+++ app/db/database.py
@@ -9,6 +9,13 @@ engine = create_async_engine(url=DATABASE_URL)
 
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
+async def get_db():
+    async with async_session_maker() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
+
 
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
