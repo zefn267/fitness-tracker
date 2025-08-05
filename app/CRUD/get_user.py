5bc849605e3a460c777cda334ce914634b63f4f7
Georgy <144234024+zefn267@users.noreply.github.com>
2025-08-05 18:17:19 +0500

    add get_user.py

---
+++ app/CRUD/get_user.py
@@ -0,0 +1,8 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.user import User
+
+async def get_user(db: AsyncSession, user_id: int):
+    result = await db.execute(select(User).where(User.id == user_id))
+
+    return result.scalar_one_or_none()
