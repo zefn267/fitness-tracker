fcf38ede25c1edd5e1c8d6851d9a3216a0a1e9ef
Georgy <144234024+zefn267@users.noreply.github.com>
2025-08-12 17:18:54 +0500

    add auth

--- app/CRUD/get_user.py
+++ app/CRUD/get_user.py
@@ -2,7 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.user import User
 
-async def get_user(db: AsyncSession, user_id: int):
-    result = await db.execute(select(User).where(User.id == user_id))
+async def get_user(db: AsyncSession, username: str):
+    result = await db.execute(select(User).where(User.user_name == username))
 
     return result.scalar_one_or_none()
---
+++ app/core/auth.py
@@ -0,0 +1,75 @@
+from datetime import timedelta, datetime, timezone
+
+import jwt
+from typing import Annotated
+
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.config import settings
+from app.CRUD.get_user import get_user
+from app.schemas.token_data import TokenData
+
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+async def authenticate_user(username: str, password: str, db: AsyncSession):
+    user = await get_user(db, username=username)
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: AsyncSession):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = await get_user(db, username=token_data.username)
+
+    if user is None:
+        raise credentials_exception
+
+    return user
